698494cccd9c4f26e1b5d42235c4344d994aa302
Himanshu Raj <hrastogi1998@gmail.com>
2020-08-07 17:23:40 +0530

    Added footer & made owl carousel responsive

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+const responsive = {
+    0:{
+        items:1
+    },
+    320:{
+        items:1
+    },
+    560:{
+        items:2
+    },
+    960:{
+        items:3
+    }
+}
+
 $(document).ready(function(){
   
     $nav = $('.nav');
@@ -16,8 +31,15 @@ $('.owl-carousel').owlCarousel({
     autoplayTimeout: 3000,
     dots:false,
     nav:true,
-    navText:[$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')]
+    navText:[$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')],
+    responsive: responsive
 });
-
     /** end */
+    /*Move up scroll*/ 
+$('.move-up span').click(function(){
+    $('html,body').animate({
+        scrollTop: 0
+    },1000);
+})
+ /*Move up scroll End*/
 });
